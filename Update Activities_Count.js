7521b2cc3c6b913945e9bc7c1a09aac91e3aca36
CubeYogi <34396887+CubeYogi@users.noreply.github.com>
2017-12-09 21:26:18 +0530

    Update Update Activities_Count.js

--- Update Activities_Count.js
+++ Update Activities_Count.js
@@ -2,7 +2,7 @@
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* STEP 1 : the current Lead's ID is converted as string and assigned here*/
-lead_identifier=input.lead_id.toString();
+lead_identifier = input.lead_id.toString();
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* STEP 2 : Call the Integration function to get the activties (Tasks, Events, Notes, Calls) related to Leads */
@@ -13,11 +13,15 @@ related_notes = zoho.crm.getRelatedRecords("Notes","Leads",lead_identifier);
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* STEP 3 : find the total number of activities took place for the Lead */
-total_no_of_activities=(related_tasks.size()  +  related_calls.size()  +  related_events.size()  +  related_notes.size());
+total_no_of_activities =
+  (  related_tasks.size() 
+   + related_calls.size() 
+   + related_events.size()  
+   + related_notes.size() );
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* STEP 4 : Assign a Map data structure to store the count of activities*/
-activity_info_map=map();
+activity_info_map = map();
 activity_info_map.put("Number of Tasks",related_tasks.size());
 activity_info_map.put("Number of Events",related_events.size());
 activity_info_map.put("Number of Calls",related_calls.size());
