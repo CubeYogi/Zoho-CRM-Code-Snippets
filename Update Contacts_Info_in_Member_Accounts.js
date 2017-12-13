5a3abb6e09328792875e21715437f03d250e606b
CubeYogi <34396887+CubeYogi@users.noreply.github.com>
2017-12-13 20:36:30 +0530

    Update Update Contacts_Info_in_Member_Accounts.js

--- Update Contacts_Info_in_Member_Accounts.js
+++ Update Contacts_Info_in_Member_Accounts.js
@@ -1,4 +1,14 @@
 /** Custom function to copy the conatct info of Parent Account to is associated Member Accounts (Child Accounts) **/
+
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**/
+/** IMPORTANT NOTE!!!  "input." are the fields that are mapped from Edit_Arguments.
+  Please click on edit argumnets in the ustom function and do the mapping as below:
+ 1. "input.parent_account_id" is mapped to "Account ID" of Accounts
+ 2. "input.parent_phone" is mapped to "Phone" of Accounts
+ 3. "input.parent_fax" is mapped to "Fax" of Accounts
+ 4. "input.parent_website" is mapped to "Webiste" of Accounts **/
+
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**/
 /** Parent Account identifier **/
 parent_account_identifer = (input.parent_account_id).toString();
 
